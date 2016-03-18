93295af7b4ffa8861a3a237310355c52de5e06a5
egon w. stemle <egon.stemle@eurac.edu>
2016-03-18 16:19:34 +0100

    empirist/representation/__init__.py - rm RepresentationError class;simplify

--- empirist/representation/__init__.py
+++
@@ -1,8 +0,0 @@
-class RepresentationError(Exception):
-    """
-    Exception raised for Errors in the Representation.
-    """
-    pass
-
-from .PosTags import PosTags
-from .QOneHotChars import QOneHotChars
