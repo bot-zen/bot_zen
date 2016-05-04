08e8bc7af727acfdcf5d315b07e62bc8efeae49b
egon w. stemle <egon.stemle@unitn.it>
2016-05-04 17:22:51 +0200

    data/12_w2v_process.sh - add more vars

--- data/12_w2v_process.sh
+++ data/12_w2v_process.sh
@@ -10,7 +10,12 @@ set -e
 [ -d ./tmp/word2vec.svn ] || svn co http://word2vec.googlecode.com/svn/trunk ./tmp/word2vec.svn
 [ -x ./tmp/word2vec.svn/word2vec ] || (pushd ./tmp/word2vec.svn; make word2vec; popd)
 
-mkdir -p word2vec
-./tmp/word2vec.svn/word2vec -train ./tmp/empirist.txt -output word2vec/empirist.bin -cbow 0 -size 500 -window 10 -hs 1 -negative 5 -min-count 3 -threads 4 -debug 2 -save-vocab ./word2vec/empirist-vocab.txt
+DATA_EMPIRIST=w2v_empirist.txt
+DATA_WIKIPEDIA=w2v_de.wikipedia.org.txt
 
-./tmp/word2vec.svn/word2vec -train ./tmp/bigdata.txt -output ./word2vec/bigdata.bin -cbow 1 -size 500 -window 10 -hs 1 -negative 3 -min-count 25 -threads 4 -debug 2 -save-vocab ./wordvec/bigdata-vocab.txt
+THREADS=${THREADS:-4}
+
+mkdir -p word2vec
+./tmp/word2vec.svn/word2vec -train ./tmp/$DATA_EMPIRIST -output word2vec/empirist.bin -cbow 0 -size 500 -window 10 -hs 1 -negative 5 -min-count 3 -threads $THREADS -debug 2 -save-vocab ./word2vec/empirist-vocab.txt
+
+./tmp/word2vec.svn/word2vec -train ./tmp/$DATA_WIKIPEDIA -output ./word2vec/bigdata.bin -cbow 1 -size 500 -window 10 -hs 1 -negative 3 -min-count 25 -threads $THREADS -debug 2 -save-vocab ./word2vec/bigdata-vocab.txt
