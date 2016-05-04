65281e90ee2cdad6466ed242e083e2925140ba31
egon w. stemle <egon.stemle@unitn.it>
2016-05-04 17:25:01 +0200

    data/10_w2v_get.sh - avoid re-download of corpus files

--- data/10_w2v_get.sh
+++ data/10_w2v_get.sh
@@ -30,19 +30,23 @@ SUBDIR=tmp/de.wikipedia.org
 mkdir -p ${SUBDIR}
 for file in wud15.tt.xml.bz2 wdd15.tt.xml.bz2 wpd15.tt.xml.bz2; do
 
-    download "${SUBDIR}"/"${file}" 'http://corpora.ids-mannheim.de/pub/wikipedia-deutsch/2015/'"${file}" \
-        && echo "Downloaded ${file}"
     [ -e "${SUBDIR}"/$(basename "${file}" .tt.xml.bz2).tt.bz2 ] \
-        || bzcat "${SUBDIR}"/"${file}" \
-        | grep -e 'sentence>' -e '<surface-form>' \
-        | sed -e 's#\s\+<sentence>#<s>#' \
-              -e 's#\s\+</sentence>#</s>#' \
-              -e 's#\s\+<surface-form>##' \
-              -e 's#</surface-form>##' \
-        > "${SUBDIR}"/$(basename "${file}" .tt.xml.bz2).tt \
-            && echo "tt-ed ${file}"
-    iconv -flatin1 -tutf8//IGNORE "${SUBDIR}"/$(basename "${file}" .tt.xml.bz2).tt \
-    | bzip2 -c -9 > "${SUBDIR}"/$(basename "${file}" .tt.xml.bz2).tt.bz2 \
+    || 
+    ( \
+        download "${SUBDIR}"/"${file}" 'http://corpora.ids-mannheim.de/pub/wikipedia-deutsch/2015/'"${file}" \
+        && echo "Downloaded ${file}" \
+        && bzcat "${SUBDIR}"/"${file}" \
+           | grep -e 'sentence>' -e '<surface-form>' \
+           | sed -e 's#\s\+<sentence>#<s>#' \
+                 -e 's#\s\+</sentence>#</s>#' \
+                 -e 's#\s\+<surface-form>##' \
+                 -e 's#</surface-form>##' \
+           > "${SUBDIR}"/$(basename "${file}" .tt.xml.bz2).tt \
+        && echo "tt-ed ${file}"
+
+        iconv -flatin1 -tutf8//IGNORE "${SUBDIR}"/$(basename "${file}" .tt.xml.bz2).tt \
+        | bzip2 -c -9 > "${SUBDIR}"/$(basename "${file}" .tt.xml.bz2).tt.bz2 \
         && echo "iconv-ed ${file}" \
-            && rm "${SUBDIR}"/$(basename "${file}" .tt.xml.bz2).tt
+        && rm "${SUBDIR}"/$(basename "${file}" .tt.xml.bz2).tt
+   )
 done
