97e48c1b70e9e54b71e3b34ae1f6f1998eb0b698
egon w. stemle <egon.stemle@eurac.edu>
2016-05-04 17:40:38 +0200

    data/{00_,01_}* - split getting empirist and tiger data

--- data/00_empirist_get.sh
+++ data/00_empirist_get.sh
@@ -2,7 +2,6 @@
 #
 # Get training, test, etc. data for EmpiriST 2016
 # * the EmpiriST 2016 data
-# * the Tiger Corpus
 
 set -e
 
@@ -36,24 +35,3 @@ pushd "${SUBDIR}"
 unzip -tq "*.zip" && unzip "*.zip"
 find ./ -name '*.txt' -exec sed -i -e '1{s/^\xef\xbb\xbf//;}' "{}" \;
 popd
-
-
-#
-### Tiger Corpus
-#
-# *important* http://www.ims.uni-stuttgart.de/forschung/ressourcen/korpora/TIGERCorpus/license/index.html
-# http://www.ims.uni-stuttgart.de/forschung/ressourcen/korpora/TIGERCorpus/download/tigercorpus-2.2.conll09.tar.gz
-#
-# * download tigercorpus-2.2, conll format 
-# * extract vertical file from conll format, i.e. lines of the form:
-#   token\tlemma\tPOS
-# * introduce <s> </s> marks
-# * subst PROAV with PAV and NNE with NE
-download tmp/tigercorpus-2.2.conll09.tar.gz 'http://www.ims.uni-stuttgart.de/forschung/ressourcen/korpora/TIGERCorpus/download/tigercorpus-2.2.conll09.tar.gz'
-[ -e tiger_release_aug07.corrected.16012013-empirist.vrt.bz2 ] \
-    || tar -xOzf tmp/tigercorpus-2.2.conll09.tar.gz tiger_release_aug07.corrected.16012013.conll09 \
-    | cut -f2,3,5 \
-    | sed -e '1{s#\(.*\)#<s>\n\1#;}' -e '${s#^$#</s>#;}' -e 's#^$#</s>\n<s>#' \
-    | sed -e 's/PROAV$/PAV/' -e 's/NNE$/NE/' \
-    | bzip2 -c -9 \
-    > tiger_release_aug07.corrected.16012013-empirist.vrt.bz2
---
+++ data/01_tiger_get.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+#
+# Get training, test, etc. data for EmpiriST 2016
+# * the Tiger Corpus
+
+set -e
+
+download() {
+    DESTDIR=$(dirname "$1")
+    DESTFN=$(basename "$1")
+    mkdir -p "${DESTDIR}"
+
+    [ -e "${DESTDIR}/${DESTFN}" ] || (wget --continue -O "${DESTDIR}/.${DESTFN}" "$2" && mv "${DESTDIR}/.${DESTFN}" "${DESTDIR}/${DESTFN}")
+}
+
+
+#
+### Tiger Corpus
+#
+# *important* http://www.ims.uni-stuttgart.de/forschung/ressourcen/korpora/TIGERCorpus/license/index.html
+# http://www.ims.uni-stuttgart.de/forschung/ressourcen/korpora/TIGERCorpus/download/tigercorpus-2.2.conll09.tar.gz
+#
+# * download tigercorpus-2.2, conll format 
+# * extract vertical file from conll format, i.e. lines of the form:
+#   token\tlemma\tPOS
+# * introduce <s> </s> marks
+# * subst PROAV with PAV and NNE with NE
+download tmp/tigercorpus-2.2.conll09.tar.gz 'http://www.ims.uni-stuttgart.de/forschung/ressourcen/korpora/TIGERCorpus/download/tigercorpus-2.2.conll09.tar.gz'
+[ -e tiger_release_aug07.corrected.16012013-empirist.vrt.bz2 ] \
+    || tar -xOzf tmp/tigercorpus-2.2.conll09.tar.gz tiger_release_aug07.corrected.16012013.conll09 \
+    | cut -f2,3,5 \
+    | sed -e '1{s#\(.*\)#<s>\n\1#;}' -e '${s#^$#</s>#;}' -e 's#^$#</s>\n<s>#' \
+    | sed -e 's/PROAV$/PAV/' -e 's/NNE$/NE/' \
+    | bzip2 -c -9 \
+    > tiger_release_aug07.corrected.16012013-empirist.vrt.bz2
