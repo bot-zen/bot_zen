847017098b252b8987c7c51bd20ee4db00b5dd94
egon w. stemle <egon.stemle@eurac.edu>
2016-09-14 13:09:59 +0200

    tag_now.py - do not model.save and process_test_data; change print to logging for some output

--- tag_now.py
+++ tag_now.py
@@ -26,25 +26,30 @@ nb_epoch = 20
 batch_size = 20
 model = network.build_nn(output_dim=postagstype_ibk.feature_length,
                          lstm_output_dim=1024, dropout=dropout)
-model.save_weights('/tmp/emptig_plain.hdf5', overwrite=True)
+# model.save_weights('/tmp/emptig_plain.hdf5', overwrite=True)
 
 network.train_nn(model, toks, tags, batch_size=batch_size,
                  nb_epoch=nb_epoch, postagstype=postagstype_ibk)
-model.save_weights('/tmp/emptig_trained-0.hdf5', overwrite=True)
+# model.save_weights('/tmp/emptig_trained-0.hdf5', overwrite=True)
 res_emp2 = network.eval_nn(model, toks_gold, tags_gold,
                            postagstype=postagstype_ibk)
-network.compact_res(res_emp2)
+logger.info(network.compact_res(res_emp2))
+retres.append(('emp', res_emp2))
+
+
+# ##
+
 batch_size = 50
 network.train_nn(model, toks_tig, tags_tig, batch_size=batch_size,
                  nb_epoch=nb_epoch, postagstype=postagstype_ibk)
-model.save_weights('/tmp/emptig_trained-1.hdf5', overwrite=True)
+# model.save_weights('/tmp/emptig_trained-1.hdf5', overwrite=True)
 res_emptig = network.eval_nn(model, toks_gold, tags_gold,
                              postagstype=postagstype_ibk)
-network.compact_res(res_emptig)
-process_test_data_tagging(model, extension=".emptig",
-                          postagstype=postagstype_ibk)
+logger.info(network.compact_res(res_emptig))
+
+# process_test_data_tagging(model, extension=".emptig",
+#                           postagstype=postagstype_ibk)
 retres.append(('emptig', res_emptig))
 
 print(retres)
-print(network.compact_res(retres[0][1]))
 exit(0)
